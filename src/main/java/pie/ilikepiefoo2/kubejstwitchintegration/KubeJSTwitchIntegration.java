b380da2da9f5bae75a8f81fbf190cb25d304ce5b
Hunter19823 <hspragg@asu.edu>
2021-04-01 09:24:02 -0700

    Setup environment.

--- src/main/java/pie/ilikepiefoo2/kubejstwitchintegration/KubeJSTwitchIntegration.java
+++ src/main/java/pie/ilikepiefoo2/kubejstwitchintegration/KubeJSTwitchIntegration.java
@@ -1,4 +1,17 @@
 package pie.ilikepiefoo2.kubejstwitchintegration;
 
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("kubejstwitchintegration")
 public class KubeJSTwitchIntegration {
+    public static final String MOD_ID = "kubejstwitchintegration";
+    public static final String MOD_NAME = "KubeJSTwitchIntegration";
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public KubeJSTwitchIntegration()
+    {
+        LOGGER.info("KubeJS Twitch Integration Initializing");
+    }
 }
